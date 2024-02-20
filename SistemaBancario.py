ca6d7a0033428d5e26616d8a91c6f09af25bf55c
kelidss <kelianedss12@gmail.com>
2024-02-20 09:29:16 -0300

    first commit

--- SistemaBancario.py
+++ SistemaBancario.py
@@ -1,37 +1,69 @@
 #aqui esta apenas uma parte da logica 
+menu = """
 
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
 
-class ContaBancaria:
-    def __init__(self, titular, saldo=0):
-        self.titular = titular
-        self.saldo = saldo
+=> """
 
-    def deposito(self, valor):
+saldo = 0
+limite = 500
+extrato = ''
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+    opcao = input(menu)
+
+    if opcao == 'd':
+        valor = float(input('informe o  valor de depósito: '))
+
         if valor > 0:
-            self.saldo += valor
-            print(f'Depósito de R${valor} realizado com sucesso.')
+            saldo += valor 
+            extrato += f'Depósito: R$ {valor:.2f}\n'
+
         else:
-            print('O valor do depósito deve ser maior que zero.')
+            print('valor informado inválido')
 
-    def saque(self, valor):
-        if 0 < valor <= self.saldo:
-            self.saldo -= valor
-            print(f'Saque de R${valor} realizado com sucesso.')
-        else:
-            print('Saldo insuficiente para realizar o saque ou valor inválido.')
+    elif opcao == 's':
+        valor = float(input('informe o valor do saque: '))
 
-    def extrato(self):
-        print(f'Titular: {self.titular}')
-        print(f'Saldo atual: R${self.saldo}')
+        excedeu_saldo = valor > saldo
 
+        excedeu_limite = valor > limite
 
+        excedeu_saque = numero_saques >= LIMITE_SAQUES
 
-if __name__ == "__main__":
-    
-    conta = ContaBancaria('keli', 1000)
+        if excedeu_saldo:
+            print('saldo insuficiente')
 
-    
-    conta.extrato()
-    conta.deposito(500)
-    conta.saque(200)
-    conta.extrato()
+        elif excedeu_limite:
+            print('o valor do saque excede seu limite')
+
+        elif excedeu_saque:
+            print('você não pode fazer mais de 3 saques')
+
+        elif valor > 0:
+            saldo -= valor
+            extrato += f'Saque: R$ {saldo:.2f}\n'
+            numero_saques += 1
+
+        else: 
+            print('Valor informado inválido')
+
+    elif opcao == 'e':
+        print('\n=============== EXTRATO ===============') 
+        print('não foram realizadas movimentações.' if not extrato else extrato)
+        print(f'\nSaldo: R$ {saldo: .2f}')
+        print('=========================================')
+
+    elif opcao == 'q':
+        break
+
+    else:
+        print('operação invalida')
+
+        
+
